1023481dd2e4921383a33567f809d707eebfa77e
Giulia Frascaria <giulia.frascaria@gmail.com>
2019-09-09 17:14:48 +0200

    test libnet

--- attack.c
+++ attack.c
@@ -4,12 +4,37 @@
 #include <sys/types>
 #include <unistd.h>
 
-#define KEVIN_IP 172.16.16.2
-#define SERVER_IP 172.16.16.3
-#define XTERMINAL_IP 172.16.16.4
+
+//eth level data -> do I need this??
+#define KEVIN_MAC "02:00:ac:10:10:02"
+#define SERVER_MAC "02:00:ac:10:10:03"
+#define XTERMINAL_MAC "02:00:ac:10:10:04"
+
+//ip level data
+#define KEVIN_IP "172.16.16.2"
+#define SERVER_IP "172.16.16.3"
+#define XTERMINAL_IP "172.16.16.4"
+
+//tcp level data
+#define PORT 513
+#define PAYLOAD_DOS "disable"
+#define PAYLOAD_RST "enable"
 
 
 int main (void)
 {
+	char e_buff[LIBNET_ERRBUF_SIZE];
+	libnet_t * l;
+
+	l = libnet_init(LIBNET_RAW4, NULL, e_buff);
+	if (l == NULL)
+	{
+		printf("libnet init error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	u_long kevin_ip;
+	u_long server_ip = libnet_name2addr4(l, SERVER_IP, LIBNET_RESOLVE);
+
 	exit(EXIT_SUCCESS);
 }
