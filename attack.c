c77c39130511c02de9520fe86e41ff38e99f885d
Giulia Frascaria <giulia.frascaria@gmail.com>
2019-09-09 16:36:17 +0200

    ip addresses

--- attack.c
+++ attack.c
@@ -1,14 +1,15 @@
 #include <libnet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types>
+#include <unistd.h>
 
-
-#define KEVIN_IP 1.1.1.1
-#define SERVER_IP 2.2.2.2
-#define XTERMINAL_IP 3.3.3.3
+#define KEVIN_IP 172.16.16.2
+#define SERVER_IP 172.16.16.3
+#define XTERMINAL_IP 172.16.16.4
 
 
 int main (void)
 {
-	
+	exit(EXIT_SUCCESS);
 }
